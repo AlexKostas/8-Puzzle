edadb5e593870efbb0520aa0c482597937125f5e
Alex Kostas <k.alexandros299@gmail.com>
2022-09-24 00:57:30 +0300

    Added move class

---
+++ Assets/Scripts/Core/Move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Move {
+    private readonly int fromIndex;
+    private readonly int toIndex;
+
+    public Move(int from, int to) {
+        Debug.Assert(from >= 0);
+        Debug.Assert(to >= 0);
+
+        fromIndex = from;
+        toIndex = to;
+    }
+
+    public int GetFrom() {
+        return fromIndex;
+    }
+
+    public int GetTo() {
+        return toIndex;
+    }
+}
